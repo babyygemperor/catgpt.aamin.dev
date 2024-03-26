24bfb80564d9c37d2054522c4a954d5f34acf13c
Aamin Gem <gem.aamin@gmail.com>
2024-03-26 15:34:35 +0100

    Add cat iamgegs

--- script.js
+++ script.js
@@ -38,26 +38,53 @@ function sendQuestion() {
 
 function animateMeowResponse(meowResponse) {
     const chatBox = document.getElementById('chat-box');
+    const messageId = `catgpt-response-${Date.now()}`;
     chatBox.innerHTML += `
         <div class="chat-message">
             <img src="catgpt.png" alt="CatGPT" class="profile-pic">
-            <div class="message-text" id="catgpt-response-${Date.now()}"></div>
+            <div class="message-text" id="${messageId}"></div>
         </div>`;
-    let catgptResponseDiv = chatBox.lastElementChild.querySelector('.message-text');
+    let catgptResponseDiv = document.getElementById(messageId);
 
-    let i = 0;
-    clearTimeout(typingTimer); // Clear previous timer
-    typingTimer = setTimeout(function typing() {
-        if (i < meowResponse.length) {
-            catgptResponseDiv.innerHTML += meowResponse.charAt(i);
-            i++;
+    const shouldDisplayCatImage = Math.random() < 0.5; // 50% chance
+
+    const typing = (message, index = 0) => {
+        if (index < message.length) {
+            catgptResponseDiv.innerHTML += message.charAt(index);
+            index++;
             let speed = Math.random() > 0.1 ? 5 : 20;
             speed *= Math.random() * 5;
-            setTimeout(typing, speed);
+            setTimeout(() => typing(message, index), speed);
         } else {
             chatBox.scrollTop = chatBox.scrollHeight;
         }
-    }, 500 + Math.random() * 1000); // Random delay before typing starts
+    };
+
+    if (shouldDisplayCatImage) {
+        fetch('https://api.thecatapi.com/v1/images/search')
+            .then(response => response.json())
+            .then(data => {
+                const catImageUrl = data[0].url;
+                // Insert the cat image with a loading animation
+                const imgElement = document.createElement('img');
+                imgElement.src = catImageUrl;
+                imgElement.alt = "Cat Image";
+                imgElement.className = "cat-image-loading"; // Apply the loading animation class
+                catgptResponseDiv.insertBefore(imgElement, catgptResponseDiv.firstChild);
+                // Insert a <br> element after the image to ensure text starts from a new line
+                const breakElement = document.createElement('br');
+                catgptResponseDiv.insertBefore(breakElement, imgElement.nextSibling);
+                catgptResponseDiv.insertBefore(breakElement, imgElement.nextSibling);
+
+                typing(meowResponse);
+            })
+            .catch(error => {
+                console.error('Error fetching cat image:', error);
+                typing(meowResponse); // Proceed to type the message even if the image fetch fails
+            });
+    } else {
+        typing(meowResponse);
+    }
 }
 
 // Event listener for Enter key
